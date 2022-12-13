06df2b76b1849e32e888099aa197821c3c54049c
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-12-13 21:27:29 +0600

    nav Bar first section is almost done

--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,36 +1,42 @@
 import React from "react";
-import './navbar.css'
+import "./navbar.css";
 import Mlogo from "../Assets/Marvel_Logo.svg";
+import Ulogo from '../Assets/icon-mu-shield.png'
 import { CiDollar } from "react-icons/ci";
 
 const NavBar = () => {
-    return (
-        <>
-        <nav className="nav_container">
-                <div className="first_section">
-                <div className="blank">
-                    blank
-                </div>
-                <div className="signIN">
-                        <CiDollar />
-                        <p>Sign in</p>
-                        <div></div>
-                        <p>join</p>
-                </div>
-                <div className="logo">
-                        <div className="img">
-                            <img src={Mlogo} alt="" />
-                    </div>
-                </div>
-                <div className="unlimited">
-                    Unlimited
-                </div>
-                <div className="blank2">
-                    Blank
-                </div>
-                </div>
-        </nav>
-        </>);
+  return (
+    <>
+      <nav className='nav_container'>
+        <div className='first_section'>
+          <div className='blank'></div>
+          <div className='signIN'>
+            <h2>
+              <CiDollar />
+            </h2>
+            <p>SIGN IN</p>
+            <div></div>
+            <p>JOIN</p>
+          </div>
+          <div className='logo'>
+            <div className='img'>
+              <img src={Mlogo} alt='' />
+            </div>
+          </div>
+          <div className='unlimited'>
+            <div className='imgU'>
+              <img src={Ulogo} alt='' />
+            </div>
+            <div className='Sub_content'>
+              <p>MARVEL UNLIMITED</p>
+              <p>SUBSSCRIBE</p>
+            </div>
+          </div>
+          <div className='blank2'></div>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default NavBar;
